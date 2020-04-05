43db0680633f43d67d50752619cf43566306571f
devakrishna33 <36110228+devakrishna33@users.noreply.github.com>
2020-04-05 21:19:26 +0530

    Configured for heroku

--- src/components/MainRoutes.js
+++ src/components/MainRoutes.js
@@ -1,29 +1,34 @@
-import React from 'react'
-import {BrowserRouter as Router,Route} from "react-router-dom";
-import Register from './Register';
-import Navbar from './Navbar'
-import "bootstrap/dist/css/bootstrap.min.css"
-import Login from './Login'
-import Profile from './Profile'
-import DoctorsList from './DoctorsList'
-import PatientDetail from './PatientDetail'
-import ZoomCall from './ZoomCall'
-import Payment from './Payment'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Register from "./Register";
+import Navbar from "./Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./Login";
+import Profile from "./Profile";
+import DoctorsList from "./DoctorsList";
+import PatientDetail from "./PatientDetail";
+import ZoomCall from "./ZoomCall";
+import Payment from "./Payment";
 
-function MainRoutes()
-{
-    return(
-        <Router>
-            <Navbar/>
-                <Route path='/Home' component={Register}/>
-                <Route path='/Login' component={Login}/>
-                <Route path='/SignUp' component={Register}/>
-                <Route path='/Profile/:id' component={Profile}/>
-                <Route path='/DocList' component={DoctorsList}/>
-                <Route path='/PatientDets/:id/:name/:slot/:fee' component={PatientDetail}/>
-                <Route path='/payment_status' component={Payment}/>
-                <Route path='/zoom_call_token/:name' component={ZoomCall}/>
-        </Router>
-    )
+function MainRoutes() {
+  return (
+    <Router>
+      <Navbar />
+      <Route path="/prescrie_deploy/Home" component={Register} />
+      <Route path="/prescrie_deploy/Login" component={Login} />
+      <Route path="/prescrie_deploy/SignUp" component={Register} />
+      <Route path="/prescrie_deploy/Profile/:id" component={Profile} />
+      <Route path="/prescrie_deploy/DocList" component={DoctorsList} />
+      <Route
+        path="/prescrie_deploy/PatientDets/:id/:name/:slot/:fee"
+        component={PatientDetail}
+      />
+      <Route path="/prescrie_deploy/payment_status" component={Payment} />
+      <Route
+        path="/prescrie_deploy/zoom_call_token/:name"
+        component={ZoomCall}
+      />
+    </Router>
+  );
 }
-export default MainRoutes
+export default MainRoutes;

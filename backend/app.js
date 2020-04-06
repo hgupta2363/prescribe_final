73791c42e6e2b412ac521e2120def859190e21e8
Neo_Code <43263087+ShubhamChaudharyy@users.noreply.github.com>
2020-04-06 13:39:43 +0530

    allow headers

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,11 @@ const key_id=process.env.KEY_ID
 const key_secret=process.env.KEY_SECRET
 var data={}
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 var dets=[]
 // var serviceAccount = require('C:/Users/chdno/Desktop/All desktop Files/PresAssignment/my-app/backend/prescribetask-firebase-adminsdk-jh3c7-d97cd99717.json');
 
